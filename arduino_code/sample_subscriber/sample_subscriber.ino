78f3915be343625695c3fbc97c57488799a90393
perfectdude719 <mohamedhussein137841@gmail.com>
2024-08-20 18:28:59 +0300

    Update sample_subscriber.ino

--- arduino_code/sample_subscriber/sample_subscriber.ino
+++ arduino_code/sample_subscriber/sample_subscriber.ino
@@ -5,9 +5,9 @@
 ros:: NodeHandle nh;
 
 //create message callback
-void write_to_servo(const std_msgs::Int32.h&msg)
+void write_to_servo(const std_msgs::Int32&msg)
 {
-  Serial.print(msg.data);
+  Serial.println(msg.data);
 }
 
 //create subscriber object
@@ -16,6 +16,7 @@ ros::Subscriber<std_msgs::Int32>sub("joystick_topic",&write_to_servo);
 void setup() {
   nh.initNode();
   nh.subscribe(sub);
+  Serial.begin(57600);
 
 }
 
