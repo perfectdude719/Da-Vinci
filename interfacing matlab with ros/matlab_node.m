2e70001a621b307e72da2f5648154fd7e6cb1848
perfectdude719 <mohamedhussein137841@gmail.com>
2024-08-22 01:04:54 +0300

    Update matlab_node.m

--- interfacing matlab with ros/matlab_node.m
+++ interfacing matlab with ros/matlab_node.m
@@ -1,13 +1,23 @@
-%create a ros subscriber 
-sub= rossubscriber('joystick_topic', 'std_msgs/Int32');
+% Initialize ROS
+%rosinit('http://<ROS_MASTER_IP>:11311'); % Replace with your ROS master's IP
 
-%recieve messages
+% Create a ROS subscriber
+sub = rossubscriber('joystick_topic', 'std_msgs/Int32');
 
+% Receive messages in a loop
 while true
-    msg =receive(sub,10);
-    
-        disp(msg.data);
-    
-pause(0.001);
+    disp('Waiting to receive a message...');
+    msg = receive(sub, 10); % Timeout of 10 seconds
+    disp('Message received.');
+
+    if ~isempty(msg)
+        disp(msg.Data); % Corrected property name
+    else
+        disp('No message received.');
+    end
+
+    pause(0.001);
 end
 
+% Shut down ROS
+rosshutdown;
